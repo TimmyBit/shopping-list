fb221d72056e14b1dca43745e8cebe0920f75700
Igor Tim <igortimushkov@gmail.com>
2025-04-15 22:51:54 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const itemForm = document.querySelector('#item-form');
+const itemInput = document.querySelector('#item-input');
+const itemList = document.querySelector('#item-list');
+
+// Add Items
+function addItem (e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+    
+    // Validate input
+    if (newItem === '') {
+        alert('Please enter an item');
+        return;
+    }
+
+    // Create a list item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+}
+
+function createButton(classes) {
+    const button = document.createElement('buttton');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+itemForm.addEventListener('submit', addItem);
