0b4c72c88489a1be2e756d931ecbfa272032377c
Igor Tim <igortimushkov@gmail.com>
2025-04-16 20:10:50 -0400

    Made the filter functional

--- js/script.js
+++ js/script.js
@@ -59,6 +59,22 @@ function clearItems () {
     checkUI();
 }
 
+// Filter
+function filterItems (e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        if (itemName.indexOf(text) != -1) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
 // Check for list items
 function checkUI () {
     const items = itemList.querySelectorAll('li');
@@ -76,5 +92,6 @@ function checkUI () {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clear.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
 
 checkUI();
